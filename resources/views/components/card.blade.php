fe0f69d6fc51dd81c94aa0beb1e1a7c608168be7
PanUcitelCz <104927244+PanUcitelCz@users.noreply.github.com>
2024-01-16 21:42:05 +0100

    Komponenty

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,23 @@
+<div>
+    <div class="Card">
+        <div class="Card-img">
+            <img src="{{ $src }}">
+        </div>
+        <div class="Card-Header">
+            <div class="Card-Header-label">
+                <h3>{{ $label }}</h3>
+            </div>
+            <div class="Card-Header-content">
+                <p>{{ $content }}</p>
+            </div>
+        </div>
+        <div class="Card-Footer">
+            <div class="alergens">
+                {{ $alergen }}
+            </div>
+            <div class="price">
+                {{ $price }}
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,15 @@
 
 @section("content")
 
-<p>sadfasdf</p>
+<div class="wrapper">
+    <div class="cards">
+        <x-card  src="https://petrstupka.cz/wp-content/uploads/2020/05/svickova-scaled.jpg" label="Svíčková" content="asdfsdfasdfasdfasdf asf asdf" alergen="1,5,3" price="150 Kč"/>
+        <x-card  src="https://petrstupka.cz/wp-content/uploads/2020/05/svickova-scaled.jpg" label="Svíčková" content="asdfsdfasdfasdfasdf asf asdf" alergen="1,5,3" price="150 Kč"/>
+        <x-card  src="https://petrstupka.cz/wp-content/uploads/2020/05/svickova-scaled.jpg" label="Svíčková" content="asdfsdfasdfasdfasdf asf asdf" alergen="1,5,3" price="150 Kč"/>
+        <x-card  src="https://petrstupka.cz/wp-content/uploads/2020/05/svickova-scaled.jpg" label="Svíčková" content="asdfsdfasdfasdfasdf asf asdf" alergen="1,5,3" price="150 Kč"/>
+        <x-card  src="https://petrstupka.cz/wp-content/uploads/2020/05/svickova-scaled.jpg" label="Svíčková" content="asdfsdfasdfasdfasdf asf asdf" alergen="1,5,3" price="150 Kč"/>
+        <x-card  src="https://petrstupka.cz/wp-content/uploads/2020/05/svickova-scaled.jpg" label="Svíčková" content="asdfsdfasdfasdfasdf asf asdf" alergen="1,5,3" price="150 Kč"/>
+    </div>
+</div>
 
 @endsection
